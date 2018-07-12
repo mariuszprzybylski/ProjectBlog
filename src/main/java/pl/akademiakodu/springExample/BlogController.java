7caa2d089f6b486358bdc3df5b84967e0033992a
Mariusz <mariusz_przybylski@wp.pl>
2018-07-12 14:50:02 +0200

    added Controller

--- src/main/java/pl/akademiakodu/springExample/BlogController.java
+++ src/main/java/pl/akademiakodu/springExample/BlogController.java
@@ -1,14 +1,29 @@
 package pl.akademiakodu.springExample;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
 
     @GetMapping("/")
-    public String add(){
+    public String add() {
         return "add";
     }
 
+    @GetMapping("/show")
+    public String show(@RequestParam String title,
+                       @RequestParam String content,
+                       @RequestParam String author,
+                         ModelMap modelMap) {
+
+        Post post = new Post(title,content,author);
+        modelMap.put("post", post);
+        return "show"; //jaki plik html ma być wyświetlony
+
+    }
+
 }
+
